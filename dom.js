245b5df507bb5c1d2232cbb52694826b763b9f7c
isaac <isaacggitonga@gmail.com>
2022-03-08 22:13:36 +0300

    Made some chenges on the files

---
+++ dom.js
@@ -0,0 +1,43 @@
+/* 
+================================================================================================================================
+EXAMINE THE  DOCUMENT OBJECT
+================================================================================================================================
+*/
+// //console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);  
+// console.log(document.all[10]);  
+// //document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+/* 
+================================================================================================================================
+getElementById
+================================================================================================================================
+*/
+
+//console.log(document.getElementById('header'));
+// var subject = document.getElementById('subject');
+// var header = document.getElementById('header');
+
+// console.log(subject);
+// subject.textContent = 'Hello';
+// subject.innerText = 'Goodbye';
+// console.log(subject.innerText);
+// subject.innerHTML = '<h1>Hello</h1>';
+// header.style.borderBottom = '3px solid #000';
+
+/* 
+================================================================================================================================
+getElelmentByClassName
+================================================================================================================================
+*/
+
