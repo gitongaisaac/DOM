bf170bd5c944fad29dbc4726d3a76b3495f6bbbc
isaac <isaacggitonga@gmail.com>
2022-03-09 07:31:49 +0300

    Made some changes

--- dom.js
+++ dom.js
@@ -41,3 +41,18 @@ getElelmentByClassName
 ================================================================================================================================
 */
 
+var items = document.getElementsByClassName('list-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+// Gives an error
+//items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < items.length; i) {
+    items[i].style.backgroundColor = '#f4f4f4';
+}
+
+
