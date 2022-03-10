ec8f69cbbe507d1c6d1c3d40331b66f7e18a5cde
isaac <isaacggitonga@gmail.com>
2022-03-10 09:47:04 +0300

    Made some chnnges on the JS file

--- dom.js
+++ dom.js
@@ -41,18 +41,150 @@ getElelmentByClassName
 ================================================================================================================================
 */
 
-var items = document.getElementsByClassName('list-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+// let items = document.getElementsByClassName('list-item');
 
-// Gives an error
-//items.style.backgroundColor = '#f4f4f4';
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
 
-for(var i = 0; i < items.length; i) {
-    items[i].style.backgroundColor = '#f4f4f4';
+// // Gives an error
+// //items.style.backgroundColor = '#f4f4f4';
+
+// for(let i = 0; i < items.length; i++) {
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+/* 
+================================================================================================================================
+getElelmentByTagName
+================================================================================================================================
+*/
+
+// let li = document.getElementsByTagName('li');
+
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+/* 
+================================================================================================================================
+querySelector  // Grabs the first item
+================================================================================================================================
+*/
+
+// let header = document.querySelector('#header');
+
+// console.log(header);
+// header.style.borderBottom = '4px solid #000';
+
+// let input = document.querySelector('input');
+// input.value = 'Hello World';
+
+// let submit = document.querySelector('input[type="submit"]');
+// submit.value = "Send"
+
+// let item = document.querySelector('.list-item');
+// item.style.color = 'red';
+
+// let lastItem = document.querySelector('.list-item:last-child');
+// lastItem.style.color = 'blue';
+
+// let secondItem = document.querySelector('.list-item:nth-child(2)');
+// secondItem.style.color = 'grey';
+
+/* 
+================================================================================================================================
+querySelectorAll
+================================================================================================================================
+*/
+
+var titles = document.querySelectorAll('.title');
+ 
+console.log(titles); 
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
 }
 
+/* 
+================================================================================================================================
+TRAVERSING THE DOM
+================================================================================================================================
+*/
 
+var itemList = document.querySelector('.items');
+console.log(itemList);
+
+/* ===== parentNode ===== */
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+
+/* ===== parentElement ===== */
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+
+
+/* ===== childNodes ===== */  // Don't use this, use children instead
+// console.log(itemList.childNodes);
+
+/* ===== children ===== */
+// console.log(itemList.children); 
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+
+
+/* ===== firstChild ===== */  // Don't use this, use firstElementchild instead
+// console.log(itemList.firstChild); 
+
+/* ===== firstElementChild ===== */
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello';
+
+
+
+/* ===== lastChild ===== */ // Don't use this, use lastElementChild instead
+// console.log(itemList.lastChild);
+
+/* ===== LastElementChild ===== */
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello';
+
+
+
+/* ===== nextSibling ===== */
+// console.log(itemList.nextSibling);
+
+/* ===== nextElementSibling ===== */
+// console.log(itemList.nextElementSibling);
+
+
+
+/* ===== previousSibling ===== */
+// console.log(itemList.previousSibling);
+
+/* ===== previousElementSibling ===== */
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+
+
+
+/* 
+================================================================================================================================
+createElement
+================================================================================================================================
+*/
