778249b3f7bf46cf3e707a2778069150dec41430
isaac <isaacggitonga@gmail.com>
2022-03-20 00:40:11 +0300

    finished part 1, 2 and 3 of the dom js, made new files for part 4

--- dom.js
+++ dom.js
@@ -111,18 +111,18 @@ querySelectorAll
 */
 // If you want to find all HTML elements that match a specified CSS selector (id, class names, types, attributes, values of attributes, etc), use the querySelectorAll() method.
 
-var titles = document.querySelectorAll('.title');
+// var titles = document.querySelectorAll('.title');
  
-console.log(titles); 
-// titles[0].textContent = 'Hello';
+// console.log(titles); 
+// // titles[0].textContent = 'Hello';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
 
-for(var i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor = '#f4f4f4';
-    even[i].style.backgroundColor = '#ccc';
-}
+// for(var i = 0; i < odd.length; i++) {
+//     odd[i].style.backgroundColor = '#f4f4f4';
+//     even[i].style.backgroundColor = '#ccc';
+// }
 
 /* 
 ================================================================================================================================
@@ -130,8 +130,8 @@ TRAVERSING THE DOM
 ================================================================================================================================
 */
 
-var itemList = document.querySelector('.items');
-console.log(itemList);
+// var itemList = document.querySelector('.items');
+// console.log(itemList);
 
 /* ===== parentNode ===== */
 // console.log(itemList.parentNode);
@@ -229,8 +229,90 @@ Events
 ================================================================================================================================
 */
 
-var button = document.querySelectorAll('.item-list .btn');
-console.log(button);
+// var button = document.getElementById('span').addEventListener('click', buttonClick);
 
-// button.style.backgroundColor = 'grey';
+// function buttonClick(e) {
+//     // console.log('Button Clicked');
+//     // document.getElementById('subject-title').textContent = 'Modified';
+//     // document.querySelector('.card').style.backgroundColor = '#f4f4f4';
 
+//     // console.log(e.target);
+//     // console.log(e.target.id);
+//     // console.log(e.target.className);
+//     // console.log(e.target.classList);
+
+//     // var output = document.getElementById('output');
+//     // output.innerHTML = '<h3>'+e.target.id+'</h3>';
+
+//     // console.log(e.type);
+
+//     // console.log(e.offsetx);
+//     // console.log(e.offsety);
+
+//     // console.log(e.clientx);
+//     // console.log(e.clienty);
+
+//     // console.log(e.altkey);
+//     // console.log(e.ctrlkey);
+//     // console.log(e.shiftkey);
+// }
+
+
+/* ===== Mouse Events ===== */
+// var button = document.getElementById('span');
+// var box = document.getElementById('box');
+
+// button.addEventListener('click', runEvent);
+// button.addEventListener('dblclick', runEvent);
+// button.addEventListener('mousedown', runEvent);
+// button.addEventListener('mouseup', runEvent);
+
+// Mouse Over is for the inner Element ie the h3
+// Mouse Enter is for the actuall element ie the box element
+
+// box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseleave', runEvent);
+
+// box.addEventListener('mouseover', runEvent);
+// box.addEventListener('mouseout', runEvent);
+
+// box.addEventListener('mousemove', runEvent);
+
+// function runEvent(e) {
+//     console.log('Event Type: '+e.type);
+
+//     // output.innerHtml = '<h3>MouseX: '+e.offsetX+'</h3><h3>MouseY: '+e.offsetY+'</h3>';
+
+//     // document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",40)";
+// }
+
+
+/* ===== KeyBoard and Input Events ===== */
+// var itemInput = document.querySelector('input[type="text"]');
+// var form = document.querySelector('form');
+// var select = document.querySelector('select');
+
+// itemInput.addEventListener('keydown', runEvent);
+// itemInput.addEventListener('keyup', runEvent);
+// itemInput.addEventListener('keypress', runEvent);
+
+// itemInput.addEventListener('focus', runEvent);
+// itemInput.addEventListener('blur', runEvent);
+
+// itemInput.addEventListener('cut', runEvent);
+// itemInput.addEventListener('paste', runEvent);
+
+// itemInput.addEventListener('input', runEvent);
+
+// select.addEventListener('change', runEvent);
+// select.addEventListener('input', runEvent);
+
+// form.addEventListener('submit', runEvent);
+
+// function runEvent(e) {
+//     e.preventDefault();
+//     console.log('Event Type: ' +e.type);
+
+//     // console.log(e.target.value);
+//     // document.getElementById('output').innerHTML = '<h3>' +e.target.value+'</h3>';
+// }
